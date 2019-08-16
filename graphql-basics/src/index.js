f69043504eb3d743f37e025469e1945f3c3495e4
Clint Losee <clint.losee@gmail.com>
2019-08-16 12:35:50 -0600

    Finished up deletion mutations.

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -2,7 +2,7 @@ import { GraphQLServer } from 'graphql-yoga'
 import uuidv4 from 'uuid/v4'
 
 //* Demo user data
-const users = [
+let users = [
   {
     id: '1',
     name: 'Clint',
@@ -17,7 +17,7 @@ const users = [
   }
 ]
 
-const posts = [
+let posts = [
   {
     id: '1',
     title: 'Post 1, Sweet!',
@@ -34,7 +34,7 @@ const posts = [
   }
 ]
 
-const comments = [
+let comments = [
   {
     id: '101',
     text: 'This is comment one.',
@@ -49,7 +49,7 @@ const comments = [
   },
   {
     id: '103',
-    text: 'Another intersting comment here.',
+    text: 'Another interesting comment here.',
     author: '2',
     post: '1'
   },
@@ -76,8 +76,11 @@ const typeDefs = `
 
   type Mutation {
     createUser(data: CreateUserInput): User!
+    deleteUser(id: ID!): User!
     createPost(data: CreatePostInput): Post!
+    deletePost(id: ID!): Post!
     createComment(data: CreateCommentInput): Comment!
+    deleteComment(id: ID!): Comment!
   }
 
   input CreateUserInput {
@@ -209,6 +212,34 @@ const resolvers = {
       return user
     },
 
+    deleteUser(parent, args, ctx, info) {
+      const userIndex = users.findIndex(user => {
+        return user.id === args.id
+      })
+
+      if (userIndex === -1) {
+        throw new Error('User not found.')
+      }
+
+      const deletedUsers = users.splice(userIndex, 1)
+
+      posts = posts.filter(post => {
+        const match = post.author === args.id
+
+        if (match) {
+          comments = comments.filter(comment => {
+            return comment.post !== post.id
+          })
+        }
+
+        return !match
+      })
+
+      comments = comments.filter(comment => comment.author !== args.id)
+
+      return deletedUsers[0]
+    },
+
     createPost(parent, args, ctx, info) {
       const userExists = users.some(user => {
         return user.id === args.data.author
@@ -226,6 +257,24 @@ const resolvers = {
       posts.push(post)
 
       return post
+    },
+
+    deletePost(parent, args, ctx, info) {
+      const postIndex = posts.findIndex(post => {
+        return post.id === args.id
+      })
+
+      if (postIndex === -1) {
+        throw new Error('Post not found.')
+      }
+
+      const deletedPosts = posts.splice(postIndex, 1)
+
+      comments = comments.filter(comment => {
+        return comment.post !== args.id
+      })
+
+      return deletedPosts[0]
     },
 
     createComment(parent, args, ctx, info) {
@@ -248,6 +297,20 @@ const resolvers = {
       comments.push(comment)
 
       return comment
+    },
+
+    deleteComment(parent, args, ctx, info) {
+      const commentIndex = comments.findIndex(comment => {
+        return comment.id === args.id
+      })
+
+      if (commentIndex === -1) {
+        throw new Error('Comment not found.')
+      }
+
+      const deletedComments = comments.splice(commentIndex, 1)
+
+      return deletedComments[0]
     }
   },
   Post: {
