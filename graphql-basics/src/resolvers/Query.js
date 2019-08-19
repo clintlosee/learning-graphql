8d9f2ef6cb22c4b24d439ca4efafce10eca1a6e9
Clint Losee <clint.losee@gmail.com>
2019-08-19 08:25:36 -0600

    Finalized refactor.

---
+++ graphql-basics/src/resolvers/Query.js
@@ -0,0 +1,31 @@
+const Query = {
+  users(parent, args, { db }, info) {
+    if (!args.query) {
+      return db.users
+    }
+
+    return db.users.filter((user) => {
+      return db.user.name.toLowerCase().includes(args.query.toLowerCase())
+    })
+  },
+  posts(parent, args, { db }, info) {
+    if (!args.query) {
+      return db.posts
+    }
+
+    return db.posts.filter((post) => {
+      return post.title.toLowerCase().includes(args.query.toLowerCase()) || post.body.toLowerCase().includes(args.query.toLowerCase())
+    })
+  },
+  comments(parent, args, { db }, info) {
+    if (!args.query) {
+      return db.comments
+    }
+
+    return db.comments.filter((comment) => {
+      return comment.text.toLowerCase().includes(args.query.toLowerCase()) || comment.id.includes(args.query)
+    })
+  },
+}
+
+export { Query as default }
