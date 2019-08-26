f58920533e418b642085afecd15a2094e7bdc455
Clint Losee <clint.losee@gmail.com>
2019-08-26 13:14:41 -0600

    Set up prisma binding and config for node

--- graphql-prisma/src/import-export/index.js
+++
@@ -1,9 +0,0 @@
-import myLocation, { message, getGreeting } from './myModule'
-import add, { subtract } from './math'
-
-// console.log(message)
-// console.log(myLocation)
-// console.log(getGreeting('Clint'))
-
-console.log(add(1, 5))
-console.log(subtract(7, 3))
