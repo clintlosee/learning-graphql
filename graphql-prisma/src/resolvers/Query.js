b14bf8d69ce7b9660c5ed5223ba1671651af6304
Clint Losee <clint.losee@gmail.com>
2019-09-20 13:18:26 -0600

    Setting up Prisma for relational data.

--- graphql-prisma/src/resolvers/Query.js
+++ graphql-prisma/src/resolvers/Query.js
@@ -1,34 +1,36 @@
 const Query = {
   users(parent, args, { prisma }, info) {
-    return prisma.query.users(null, info)
+    const opArgs = {}
 
-    // if (!args.query) {
-    //   return db.users
-    // }
-
-    // return db.users.filter((user) => {
-    //   return db.user.name.toLowerCase().includes(args.query.toLowerCase())
-    // })
-  },
-  posts(parent, args, { prisma }, info) {
-    return prisma.query.posts(null, info)
-
-    // if (!args.query) {
-    //   return db.posts
-    // }
-
-    // return db.posts.filter((post) => {
-    //   return post.title.toLowerCase().includes(args.query.toLowerCase()) || post.body.toLowerCase().includes(args.query.toLowerCase())
-    // })
-  },
-  comments(parent, args, { db }, info) {
-    if (!args.query) {
-      return db.comments
+    if (args.query) {
+      opArgs.where = {
+        OR: [{
+          name_contains: args.query
+        }, {
+          email_contains: args.query
+        }]
+      }
     }
 
-    return db.comments.filter((comment) => {
-      return comment.text.toLowerCase().includes(args.query.toLowerCase()) || comment.id.includes(args.query)
-    })
+    return prisma.query.users(opArgs, info)
+  },
+  posts(parent, args, { prisma }, info) {
+    const opArgs = {}
+
+    if (args.query) {
+      opArgs.where = {
+        OR: [{
+          title_contains: args.query
+        }, {
+          body_contains: args.query
+        }]
+      }
+    }
+
+    return prisma.query.posts(opArgs, info)
+  },
+  comments(parent, args, { prisma }, info) {
+    return prisma.query.comments(null, info)
   },
 }
 
