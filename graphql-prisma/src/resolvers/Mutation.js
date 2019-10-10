b5bb89c44ce200545a4883ac60f10d74bba5d33c
Clint Losee <clint.losee@gmail.com>
2019-10-10 11:19:32 -0600

    Set up token expiration

--- graphql-prisma/src/resolvers/Mutation.js
+++ graphql-prisma/src/resolvers/Mutation.js
@@ -1,6 +1,6 @@
 import bcrypt from 'bcryptjs'
-import jwt from 'jsonwebtoken'
 import getUserId from '../utils/getUserId'
+import generateToken from '../utils/generateToken'
 
 const Mutation = {
   async createUser(parent, args, { prisma }, info) {
@@ -25,7 +25,7 @@ const Mutation = {
 
     return {
       user,
-      token: jwt.sign({ userId: user.id }, 'thisisasecret')
+      token: generateToken(user.id)
     }
   },
 
@@ -77,11 +77,9 @@ const Mutation = {
       throw new Error(`Unable to login`) //* Be generic
     }
 
-    const token = jwt.sign({ userId: user.id }, 'thisisasecret')
-
     return {
       user,
-      token
+      token: generateToken(user.id)
     }
   },
 
