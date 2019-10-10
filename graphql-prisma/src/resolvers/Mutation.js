cae5f0b29ba6078a18eb769051d09927f4c30ce3
Clint Losee <clint.losee@gmail.com>
2019-10-10 14:37:42 -0600

    Added password change functionality

--- graphql-prisma/src/resolvers/Mutation.js
+++ graphql-prisma/src/resolvers/Mutation.js
@@ -1,6 +1,7 @@
 import bcrypt from 'bcryptjs'
 import getUserId from '../utils/getUserId'
 import generateToken from '../utils/generateToken'
+import hashPassword from '../utils/hasPassword'
 
 const Mutation = {
   async createUser(parent, args, { prisma }, info) {
@@ -10,11 +11,7 @@ const Mutation = {
       throw new Error('Email taken')
     }
 
-    if (args.data.password.length < 8) {
-      throw new Error('Password must be 8 characters or longer.')
-    }
-
-    const password = await bcrypt.hash(args.data.password, 10)
+    const password = await hashPassword(args.data.password)
 
     const user = await prisma.mutation.createUser({
       data: {
@@ -35,6 +32,10 @@ const Mutation = {
 
     if (!userExists) {
       throw new Error('User not found')
+    }
+
+    if (typeof args.data.password === 'string') {
+      args.data.password = await hashPassword(args.data.password)
     }
 
     return prisma.mutation.updateUser({
